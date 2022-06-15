a05518b6bb3bfb0cf2c5d01db3d047c3734b6746
FutureIntent <IamSoulfuller@gmail.com>
2022-06-15 17:22:09 +0300

    adaptive fixes

--- src/context/back_end_url.js
+++ src/context/back_end_url.js
@@ -4,7 +4,7 @@ export const URLContext = createContext();
 
 export const URLContextProvider = (props) => {
 
-    const BackEnd_URL = 'http://localhost:3001';
+    const BackEnd_URL = 'http://83.177.140.69:3001';
 
     return (
         <URLContext.Provider value={BackEnd_URL}>
